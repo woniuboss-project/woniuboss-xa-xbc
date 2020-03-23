7d08c8b1d20bb3da11a9235d47553ecc3c12306a
XCHENG <m13709113328@163.com>
2020-03-23 18:41:28 +0800

    woniuboss代码

---
+++ WoniuBoss_project/python/WoniuBoss/tools/service.py
@@ -0,0 +1,87 @@
+
+
+from WoniuBoss.tools.utility import Utility
+import requests
+class Service:
+
+    # 判断页面上的某个元素是否存在
+    @classmethod
+    def is_element_present(cls, driver, how, what):
+        from selenium.common.exceptions import NoSuchElementException
+        try:
+            driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            # 表示没找到
+            return False
+        return True
+
+    # 向一个文本输入框执行三个固定操作：点击、清理、输入
+    @classmethod
+    def send_input(cls, ele, value):
+        ele.click()
+        ele.clear()
+        ele.send_keys(value)
+
+    # 随机选择下拉框中的一项
+    @classmethod
+    def select_random(cls, selecter):  # selecter是传递的下拉框元素
+        from selenium.webdriver.support.select import Select
+        seleter_length = len(Select(selecter).options)
+        import random
+        random_index = random.randint(0, seleter_length - 1)
+        Select(selecter).select_by_index(random_index)
+
+    # 去掉某个元素的只读属性（id）
+    @classmethod
+    def remove_readonly(cls, driver, ele_id):
+        driver.execute_script('document.getElementById("%s").readOnly=false' % (ele_id))
+
+
+    # 生成driver
+    @classmethod
+    def get_driver(cls, path):
+        contents = Utility.get_json(path)
+        from selenium import webdriver
+        driver = getattr(webdriver, contents['BROWSER'])()
+        driver.implicitly_wait(10)
+        driver.maximize_window()
+        return driver
+
+    # 打开页面的方法
+    @classmethod
+    def open_page(cls, driver, path):
+        contents = Utility.get_json(path)
+        URL = 'http://%s:%s/%s' % (contents['HOSTNAME'], contents['PORT'],contents['AURL'])
+        driver.get(URL)
+
+    # 具体的业务功能需要绕过登录，使用cookie
+    @classmethod
+    def miss_login(cls, driver, base_config_path):
+        cls.open_page(driver, base_config_path)
+        # 通过字典方式传递cookie信息
+        from agilone.tools.utility import Utility
+        contents = Utility.get_json(base_config_path)
+        driver.add_cookie({'name': 'username', 'value': contents['username']})
+        driver.add_cookie({'name': 'password', 'value': contents['password']})
+        cls.open_page(driver, base_config_path)
+
+    
+
+    def get_session(cls):
+        base_info = Utility.get_json('..\\config\\base.conf')
+        login_url = "%s://%s:%s/%s/" % (
+        base_info['PROTOCOL'], base_info['HOSTNAME'], base_info['PORT'], base_info['AURL'],)
+        # print(login_url)
+        login_data = {'username': base_info['USERNAME'], 'password': base_info['PASSWORD'],
+                      'verifycode': base_info['VERIFYCODE']}
+        # print(login_data)
+        session = requests.session()
+        session.post(login_url, login_data)
+        # print(a.text)
+        return session
+
+if __name__ == '__main__':
+    a=Service.get_driver("..\\config\\base.conf")
+    Service.open_page(a,"..\\config\\base.conf")
+
+
---
+++ WoniuBoss_project/python/WoniuBoss/tools/utility.py
@@ -0,0 +1,96 @@
+import requests
+class Utility:
+
+    # 读取文件
+    @classmethod
+    def get_json(cls, path):
+        import json
+        with open(path) as file:
+            cotents = json.load(file)
+        return cotents
+
+    # 从excel中读取内容为[{},{},{}]
+    @classmethod
+    def get_excel_to_json(clS, file_info):
+        import xlrd
+        workbook = xlrd.open_workbook(file_info['DATAPATH'])
+        contents = workbook.sheet_by_name(file_info['SHEETNAME'])
+        test_info = []
+        for i in range(file_info['STARTROW'], file_info['ENDROW']):
+            temp = contents.cell(i, file_info['DATACOL']).value
+            expc = contents.cell(i, file_info['EXPECTCOL']).value
+            list_one = temp.split('\n')
+            dict_new = {}
+            for t in list_one:
+                dict_new[t.split('=')[0]] = t.split('=')[1]
+            dict_new['expect'] = expc
+            test_info.append(dict_new)
+        return test_info
+
+
+    #GUI方式
+    # 把test_info数据转换成[(),(),()]
+    @classmethod
+    def get_excel_GUI_tuple(cls, file_info):
+        result = cls.get_excel_to_json(file_info)
+        print(result)
+        li = []
+        for i in result:
+            tup = tuple(i.values())
+            li.append(tup)
+        return li
+
+    
+    #接口
+    # 从excel中读取内容，读取结果为[{},{},{}]
+    @classmethod
+    def get_excel_port_dict(cls,xls_file_info):
+        import xlrd
+        workbook = xlrd.open_workbook(xls_file_info['DATAPATH'])
+        contents = workbook.sheet_by_name(xls_file_info['SHEETNAME'])
+        test_info = []
+        # 按行读取每一条测试信息
+        for i in range(xls_file_info['STARTROW'],xls_file_info['ENDROW']):
+            # 读取单元格中的内容
+            url=contents.cell(i,xls_file_info['URLCOL']).value
+            #method=contents.cell(i,xls_file_info['METHODCOL']).value
+            data=contents.cell(i,xls_file_info['DATACOL']).value
+            temp=data.split('\n')
+            #字典的测试数据
+            d={}
+            for t in temp:
+                d[t.split('=')[0]]=t.split('=')[1]
+            #status_code=contents.cell(i,xls_file_info['CODECOL']).value
+            resp_content=contents.cell(i,xls_file_info['CONTENTCOL']).value
+            info={'URL':url,'DATA':d,'CONTENT':resp_content}
+            test_info.append(info)
+        print(test_info)
+        return test_info
+
+
+
+
+    #读取账号信息
+    @classmethod
+    def get_tuple(cls,path):
+        result = cls.get_json(path)
+        # print(result)
+        li = []
+        for i in result:
+            tup = tuple(i.values())
+            li.append(tup)
+        return li
+
+if __name__ == '__main__':
+    a=Utility.get_json("..\\config\\testdata.conf")
+    b=Utility.get_excel_GUI_tuple(a[0])
+    c=Utility.get_excel_port_dict(a[1])
+    print(b)
+    print(c)
+    acc=Utility.get_json("..\\config\\Account.conf")
+    print(acc[0])
+    d=Utility.get_tuple("..\\config\\Account.conf")
+    print(d)
+    # d=Utility.get_json("..\\config\\base.conf")
+    # c=Utility.get_driver("..\\config\\base.conf")
+    # Utility.open_page(c,d)
