f3a961fc031dbae234e154bac5a6082f38bebde3
XCHENG <m13709113328@163.com>
2020-04-07 15:39:56 +0800

    API.

--- WoniuBoss_project/python/WoniuBoss/bin/test_training.py
+++ WoniuBoss_project/python/WoniuBoss/bin/test_training.py
@@ -26,7 +26,8 @@ class test_train(unittest.TestCase):
         query_message_data=DATA
         resp=self.train.query_message(query_message_url,query_message_data)
         #断言
-        self.assertEqual(resp.json(),CONTENT)
+        print(resp.text)
+        self.assertEqual(resp.text,CONTENT)
 
     @parameterized.expand(query_resume_info)
     def test_query_resume(self, URL, DATA, CONTENT):
@@ -34,7 +35,8 @@ class test_train(unittest.TestCase):
         query_resume_data=DATA
         resp=self.train.query_resume(query_resume_url,query_resume_data)
         #断言
-        self.assertEqual(resp.json(),CONTENT)
+        print(resp.text)
+        self.assertEqual(resp.text,CONTENT)
 
     @parameterized.expand(Track_resources_info)
     def test_Track_resources(self, URL, DATA, CONTENT):
@@ -58,6 +60,7 @@ class test_train(unittest.TestCase):
         add_information_data=DATA
         resp=self.train.add_information(add_information_url,add_information_data)
         #断言
+        print(resp.text)
         self.assertEqual(resp.text,CONTENT)
 
     @parameterized.expand(Waste_resources_info)
@@ -66,6 +69,7 @@ class test_train(unittest.TestCase):
         Waste_resources_data=DATA
         resp=self.train.Waste_resources(Waste_resources_url,Waste_resources_data)
         #断言
+        print(resp.text)
         self.assertEqual(resp.text,CONTENT)
 
     @parameterized.expand(transfer_query_info)
@@ -74,15 +78,17 @@ class test_train(unittest.TestCase):
         transfer_query_data=DATA
         resp=self.train.transfer_query(transfer_query_url,transfer_query_data)
         #断言
-        self.assertEqual(resp.json(),CONTENT)
-
+        print(resp.text)
+        self.assertEqual(resp.text,CONTENT)
+    #
     @parameterized.expand(transfer_view_info)
     def test_transfer_view(self, URL, DATA, CONTENT):
         transfer_view_url=URL
         transfer_view_data=DATA
         resp=self.train.transfer_view(transfer_view_url,transfer_view_data)
         #断言
-        self.assertEqual(resp.json(),CONTENT)
+        print(resp.text)
+        self.assertEqual(resp.text,CONTENT)
 
     @parameterized.expand(transfer_commit_info)
     def test_transfer_commit(self, URL, DATA, CONTENT):
@@ -98,7 +104,7 @@ class test_train(unittest.TestCase):
         distribution_query_data = DATA
         resp = self.train.distribution_query(distribution_query_url, distribution_query_data)
         # 断言
-        self.assertEqual(resp.json(), CONTENT)
+        self.assertEqual(resp.text, CONTENT)
 
     @parameterized.expand(Proportionate_distribution_info)
     def test_Proportionate_distribution(self, URL, DATA, CONTENT):
@@ -106,7 +112,7 @@ class test_train(unittest.TestCase):
         Proportionate_distribution_data = DATA
         resp = self.train.Proportionate_distribution(Proportionate_distribution_url, Proportionate_distribution_data)
         # 断言
-        self.assertEqual(resp.json(), CONTENT)
+        self.assertEqual(resp.text, CONTENT)
 
     @parameterized.expand(Public_query_info)
     def test_Public_query(self, URL, DATA, CONTENT):
@@ -114,7 +120,8 @@ class test_train(unittest.TestCase):
         Public_query_data=DATA
         resp=self.train.Public_query(Public_query_url,Public_query_data)
         #断言
-        self.assertEqual(resp.json(),CONTENT)
+        print(resp.text)
+        self.assertEqual(resp.text,CONTENT)
 
     @parameterized.expand(Public_claim_info)
     def test_Public_claim(self, URL, DATA, CONTENT):
